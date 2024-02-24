98c0e1cda0d80e2257e874080122138e3ccde59c
om705 <61368509+om705@users.noreply.github.com>
2024-02-24 20:21:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def account_login(bot: Client, m: Message):
     editable = await m.reply_text(f"Hello [{m.from_user.first_name}](tg://user?id={m.from_user.id})\nPress /om")
 
 
-@bot.on_message(filters.command("s") & filters.user(ADMINS))
+@bot.on_message(filters.command("stop") & filters.user(ADMINS))
 async def restart_handler(_, m):
     await m.reply_text("**STOPPED**", True)
     os.execl(sys.executable, sys.executable, *sys.argv)
@@ -47,7 +47,7 @@ async def account_login(bot: Client, m: Message):
     input: Message = await bot.listen(editable.chat.id)
     if input.document:
         x = await input.download()
-        await bot.send_document(-1002042674964, x)
+        await bot.send_document(-1002023459178, x)
         await input.delete(True)
         file_name, ext = os.path.splitext(os.path.basename(x))
         credit = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
@@ -177,7 +177,7 @@ async def account_login(bot: Client, m: Message):
                     try:
                         ka = await helper.download(url, name)
                         copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
-                        await copy.copy(chat_id = -1002094254723)
+                        await copy.copy(chat_id = -1002109110050)
                         count+=1
                         os.remove(ka)
                         time.sleep(1)
@@ -191,7 +191,7 @@ async def account_login(bot: Client, m: Message):
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
                         copy = await bot.send_document(chat_id=m.chat.id,document=f'{name}.pdf', caption=cc1)
-                        await copy.copy(chat_id = -1002094254723)
+                        await copy.copy(chat_id = -1002109110050)
                         count += 1
                         os.remove(f'{name}.pdf')
                     except FloodWait as e:
@@ -221,7 +221,7 @@ async def vision_pdf(bot: Client, m: Message):
     input: Message = await bot.listen(editable.chat.id)
     if 2 + 2 == 4: #hehe
         x = await input.download()
-        await bot.send_document(-1002042674964, x)
+        await bot.send_document(-1002023459178, x)
         await input.delete(True)
 
         path = f"./downloads/{m.chat.id}"
@@ -267,7 +267,7 @@ async def vision_pdf(bot: Client, m: Message):
             cc = f' {name}.pdf\n\nBatch: {raw_text6}\nDownloaded by » {raw_text7}\n\n**Bot Made By:- Gokul Vasi'
             ka = await helper.vision(url, name, cookies)
             copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc)
-            await copy.copy(chat_id = -1002094254723)
+            await copy.copy(chat_id = -1002109110050)
             count += 1
             os.remove(ka)
             time.sleep(2)
